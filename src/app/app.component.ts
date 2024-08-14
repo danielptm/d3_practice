5b283ca63d1a45387c675947a98ffd86b21715aa
= <danielptm@me.com>
2024-08-13 21:14:35 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {selection} from 'd3'
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'd3-practice';
+
+  hi(){
+    console.log(selection)
+  }
 }
